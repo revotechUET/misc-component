0c39c84fc80df67cc42d547e0e8489409cbcb3f5
Tung HOANG <tung.hoang@gmail.com>
2019-04-10 22:39:44 +0700

    add katex-view

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 require('./wi-icons');
 module.exports = {
-//    showEditField: require('./show-edit-field/show-edit-field'),
     editable: require('./editable'),
     sideBar : require('./side-bar/index'),
     wiBaseTreeview : require('./wi-base-treeview/wi-base-treeview'),
     wiLogin: require('./wi-login/wi-login'),
-//    mapView: require('./map-view/map-view'),
     wiToken: require('./wi-token/index'),
-    wiSession: require('./wi-session')
+    wiSession: require('./wi-session'),
+    katexView: require('./katex-view')
 }
