483b2bba47d8dbcafbd0cdfdd244254952b1bfc0
Nguyễn Đức Thắng <nguyenducthang.uet@gmail.com>
2020-01-07 11:25:34 +0700

    update

--- src/chart-widget/index.js
+++ src/chart-widget/index.js
@@ -1,4 +1,5 @@
 const componentName = 'chartWidget';
+require('../../bower_components/chartjs-plugin-labels/build/chartjs-plugin-labels.min');
 //require('angular-chart.js');
 angular.module(componentName, ['chart.js', 'editable', 'wiDropdownList']).component(componentName, {
     template: require('./template.html'),
@@ -45,9 +46,50 @@ function WidgetController($scope, $element, chartSettings) {
         }
         return widgetConfig.series;
     }
+
     const nullObj = {};
+    const pie_chart_options = {
+        plugins: {
+            labels: [
+                {
+                    render: 'label',
+                    position: 'outside'
+                },
+                {
+                    render: 'value'
+                }
+            ]
+        }
+    }
+    const bar_chart_options = {
+        plugins: {
+            labels: {
+                render: 'value'
+            }
+        }
+    }
+    const drawNullFn = () => ""
     this.getOptions = function(widgetConfig) {
-        return widgetConfig.options || nullObj;
+        switch (widgetConfig.type) {
+            case 'bar': case 'horizontal-bar':
+                if (widgetConfig.chart_options && widgetConfig.chart_options.showSegmentLabel) {
+                    bar_chart_options.plugins.labels.render = 'value' 
+                } else {
+                    bar_chart_options.plugins.labels.render = drawNullFn 
+                }
+                return Object.assign(bar_chart_options, widgetConfig.bar_chart_options);
+            case 'pie': case 'doughnut':
+                if (widgetConfig.chart_options && widgetConfig.chart_options.showSegmentLabel) {
+                    pie_chart_options.plugins.labels[0].render = 'label' 
+                    pie_chart_options.plugins.labels[1].render = "value"
+                } else {
+                    pie_chart_options.plugins.labels[0].render = drawNullFn 
+                    pie_chart_options.plugins.labels[1].render = drawNullFn 
+                }
+                return pie_chart_options;
+            default:
+                return nullObj;
+        }
     }
     this.deleteChart = function() {
         self.removeFn && self.removeFn(self.widgetConfig);
