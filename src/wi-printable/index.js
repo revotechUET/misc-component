d03918fe8a0faf8dd986d7b040203ccac102e707
Quang LN <quangln2810@gmail.com>
2021-08-21 16:51:19 +0700

    fix printable

--- src/wi-printable/index.js
+++ src/wi-printable/index.js
@@ -171,10 +171,10 @@ function PrintableCtrl($scope, $element, $timeout, $compile, wiApi, wiLoading) {
                 <span ng-bind="$ctrl.getPrintInfo()"></span>
                 <button ng-click="$ctrl.exitPreview()">Close</button>
                 <button ng-click="$ctrl.doPrint()" ng-if="$ctrl.showCtrlPage || !$ctrl.isPageBreak">Print</button>
+                <button ng-click="$ctrl.doPrintAll($ctrl, 'download')" ng-show="$ctrl.isPageBreak">Print All</button>
+                <button ng-click="$ctrl.doPrintAll($ctrl, 'save')">Save to DB</button>
                 <div style="display: inline-block;"
                 ng-if="$ctrl.showCtrlPage && $ctrl.isPageBreak">
-                    <button ng-click="$ctrl.doPrintAll($ctrl, 'download')">Print All</button>
-                    <button ng-click="$ctrl.doPrintAll($ctrl, 'save')">Save to DB</button>
                     <button ng-click="$ctrl.firstPage($ctrl)">First Page</button>
                     <button ng-click="$ctrl.previousPage($ctrl)">Previous</button>
                     <editable style="display:inline-block;" enabled='true' item-value="$ctrl.getPageIdx" set-value="$ctrl.setPageIdx" content-style='{
